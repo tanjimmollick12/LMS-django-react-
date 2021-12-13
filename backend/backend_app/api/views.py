15cfe125c82bd8c21d607d95ae06afc2b1eb05c2
Tanjim Mollik <tanzim0627@icloud.com>
2021-12-13 15:25:23 +0600

    deleted error setup

--- backend/backend_app/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-
-# Create your views here.
-def home(request):
-    return JsonResponse({
-            'info':'CSE-299',
-            'name':'group1'
-
-    })
